e32b0c06f54663fa15a4902f8a75ce1ec9e875c4
Yuan (Daniel) Li <86581841+AceFlanker@users.noreply.github.com>
2021-11-01 02:14:09 -0700

    Add new route to query for 3 upcoming user/friend trips and add a basic container for the upcoming trips

--- react-front-end/src/components/Dashboard/FutureTrips.js
+++ react-front-end/src/components/Dashboard/FutureTrips.js
@@ -1,11 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const TripContainer = styled.div`
+  background-color: #F3F5FA;
+  width: 14rem;
+  height: 14rem;
+  left: 30vw;
+  top: 10vh;
+  position: absolute;  
+  box-shadow: 2px 2px 7px 0px rgb(166, 166, 166);
+`;
 
 export const FutureTrips = function() {
   return (
-    <div className="container">
-      <h1 className="text-center" style={{ paddingTop: "30%" }}>
-        Dashboard
-      </h1>
-    </div>
+    <>
+      <TripContainer>
+        <div className="dashboard-trip-major">
+
+        </div>
+      </TripContainer>
+    </>
   )
 }
--- react-front-end/src/pages/Dashboard.js
+++ react-front-end/src/pages/Dashboard.js
@@ -1,11 +1,35 @@
 
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import WeatherApp from '../components/Weather/WeatherApp';
 import './Styles/Dashboard.scss';
+import { FutureTrips } from '../components/Dashboard/FutureTrips';
 
 const Dashboard = () => {
+
+  const [friendlist, setFriendlist] = useState([]);
+  
+  useEffect(()=>{
+    const user_id=localStorage.getItem('user_id');
+    axios.get(`api/friendlist/${user_id}`)
+    .then((res)=>{
+      setFriendlist([...res.data]);
+    })
+  }, [])
+  
+  useEffect(()=>{
+    const user_id=localStorage.getItem('user_id');
+    axios.get(`api/trips/dashboard/${user_id}`)
+    .then((res)=>{
+      console.log("UPCOMING TRIPS ", res.data);
+    })
+  }, [])
+
+ 
+  
   return (
     <div className="container">
+      <FutureTrips />
       <div className="weather-app-container">
         <WeatherApp />
       </div>
