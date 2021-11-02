d42e7d8b8e6caf6a12733a50d510d09e36461993
Babak Shirvani <bk.shirvani@gmail.com>
2021-11-02 15:11:27 -0700

    refactor the code

--- react-front-end/src/components/GearList/TripGearList.js
+++ react-front-end/src/components/GearList/TripGearList.js
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './TripGearList.css'
 import axios from 'axios';
-import { Link, useLocation } from 'react-router-dom';
-
+import { useLocation } from 'react-router-dom';
+import ListAccordion from './ListAccordion';
 
 const TripGearList = () => {
-  const [isActive, setIsActive] = useState(false);
-  const [isComplete, setComplete] = useState("");
+
   const [trip, setTrip] = useState(0);
   const [gearList, setGearList] = useState([]);
   const history = useLocation();
 
 
   useEffect(() => {
-    console.log("history44", history)
     axios.get(history.pathname)
       .then((res) => {
         setTrip(res.data[0].trip_id)
@@ -46,70 +44,17 @@ const TripGearList = () => {
         createGearObject()
       })
 
-
   }, [])
-
-
-  const handler = (gear) => {
-    if (!gear.checked && gear.id) {
-      gear.checked = true
-      setComplete(makeid(5))
-      axios.put(`/api/userGear/update/${trip}`,
-        {
-          "type_id": gear.id,
-          "checked": true
-        }
-      ).then(res => {
-        console.log("res TRUE", res)
-      })
-    }
-    else {
-      gear.checked = false
-      setComplete(makeid(5))
-      axios.put(`/api/userGear/update/${trip}`,
-        {
-          "type_id": gear.id,
-          "checked": false
-        }
-      ).then(res => {
-      })
-    }
-
-  }
-
-  function makeid(length) {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() *
-        charactersLength));
-    }
-    return result;
-  }
-
 
 
   return (
     <>
-      <div className="accordion-item" >
-        {gearList.map((item, index) => (
-
-          <div key={index}>
-            <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-              <div>{item.category}</div>
-              <div>{isActive ? '-' : '+'}</div>
-            </div>
-            {item.gears.map((gear) => (
-              isActive &&
-              <div style={{
-                textDecoration: gear.checked && 'line-through'
-              }} className="accordion-content" key={gear.id} onClick={() => handler(gear)}>
-                {gear.name}
-              </div>
-            ))}
-
-          </div>
+      <div className="accordion" >
+        {gearList.map((item) => (
+          <>
+            {console.log("0999:", item)}
+            < ListAccordion key={item.id} category={item.category} gears={item.gears} tripID={trip} />
+          </>
 
         ))}
       </div>
